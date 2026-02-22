37bae88f8878e5104e3a77b007a85f6c02177dff
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2026-02-22 19:02:54 +0530

    updated

--- EEGPipeline.py
+++ EEGPipeline.py
@@ -25,19 +25,18 @@ from torch.utils.data import DataLoader, TensorDataset, WeightedRandomSampler
 from sklearn.metrics import f1_score, classification_report
 
 # # Import data loading functions from separate module
-# from eeg_data_loader import (
-#     load_eeg_data,
-#     extract_eeg_features,
-#     create_data_splits
-# )
-
-# Import data loading functions from separate module
-from eeg_data_loader_emognitionRaw import (
+from eeg_data_loader import (
     load_eeg_data,
     extract_eeg_features,
-    create_data_splits,
-    check_json_structure
+    create_data_splits
 )
+
+# Import data loading functions from separate module
+# from eeg_data_loader_emognitionRaw import (
+#     load_eeg_data,
+#     extract_eeg_features,
+#     create_data_splits,
+# )
 
 # ==================================================
 # CONFIGURATION
@@ -46,8 +45,8 @@ from eeg_data_loader_emognitionRaw import (
 class Config:
     """EEG-specific configuration."""
     # Paths
-    # DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
-    DATA_ROOT = "/kaggle/input/datasets/ruchiabey/emognition"
+    DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
+    # DATA_ROOT = "/kaggle/input/datasets/ruchiabey/emognition"
     
     # Common parameters
     NUM_CLASSES = 4
