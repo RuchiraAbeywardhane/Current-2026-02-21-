a904bf3946e805c6c32176ce22431779788e7d9f
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2026-02-23 13:41:44 +0530

    Updated

--- EEGPipeline.py
+++ EEGPipeline.py
@@ -45,9 +45,9 @@ from eeg_data_loader import (
 class Config:
     """EEG-specific configuration."""
     # Paths
-    DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
+    # DATA_ROOT = "/kaggle/input/datasets/nethmitb/emognition-processed/Output_KNN_ASR"
     # DATA_ROOT = "/kaggle/input/datasets/ruchiabey/emognition"
-    # DATA_ROOT = "/kaggle/input/datasets/ruchiabey/asr-outputv2-0/ASR_output"
+    DATA_ROOT = "/kaggle/input/datasets/ruchiabey/asr-outputv2-0/ASR_output"
     
     # Common parameters
     NUM_CLASSES = 4
