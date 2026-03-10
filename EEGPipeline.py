fced7253a6defdb884fce1ce3e1ba1b3c34b0e65
RuchiraAbeywardhane <ruchiraabeywardhane08@gmail.com>
2026-03-10 12:13:16 +0530

    updated

--- EEGPipeline.py
+++ EEGPipeline.py
@@ -30,19 +30,19 @@ from eeg_config import Config
 # Import model architecture
 from eeg_bilstm_model import SimpleBiLSTMClassifier
 
-# # # Import data loading functions from separate module
-# from eeg_data_loader import (
-#     load_eeg_data,
-#     extract_eeg_features,
-#     create_data_splits
-# )
-
 # # Import data loading functions from separate module
-from eeg_data_loader_emognitionRaw import (
+from eeg_data_loader import (
     load_eeg_data,
     extract_eeg_features,
     create_data_splits
 )
+
+# # # Import data loading functions from separate module
+# from eeg_data_loader_emognitionRaw import (
+#     load_eeg_data,
+#     extract_eeg_features,
+#     create_data_splits
+# )
 
 # Import training functions from separate module
 from eeg_trainer import train_eeg_model
